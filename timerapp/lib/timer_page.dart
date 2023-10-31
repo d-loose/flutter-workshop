10d12b5dc0802d5a4209ed442c789a0a6c1f8d47
Dennis Loose <dennis.loose@canonical.com>
2023-10-31 16:07:20 +0100

    display remaining time in UI

--- timerapp/lib/timer_page.dart
+++ timerapp/lib/timer_page.dart
@@ -1,4 +1,7 @@
 import 'package:flutter/material.dart';
+import 'package:intl/intl.dart';
+import 'package:provider/provider.dart';
+import 'package:timerapp/timer_model.dart';
 import 'package:yaru_widgets/widgets.dart';
 
 class TimerPage extends StatelessWidget {
@@ -6,7 +9,10 @@ class TimerPage extends StatelessWidget {
   const TimerPage({super.key});
 
   static Widget create(BuildContext context) {
-    return const TimerPage();
+    return ChangeNotifierProvider(
+      create: (_) => TimerModel(),
+      builder: (_, __) => const TimerPage(),
+    );
   }
 
   static const hoursColumnKey = Key('hours column');
@@ -15,28 +21,30 @@ class TimerPage extends StatelessWidget {
 
   @override
   Widget build(BuildContext context) {
+    final model = context.watch<TimerModel>();
+
     return Scaffold(
       appBar: const YaruWindowTitleBar(title: Text('Timer App')),
       body: Center(
         child: Column(
           mainAxisSize: MainAxisSize.min,
           children: [
-            const Row(
+            Row(
               mainAxisSize: MainAxisSize.min,
               children: [
                 _TimerColumn(
                   key: hoursColumnKey,
-                  value: 12,
+                  value: model.remaining.inHours,
                 ),
-                Text(':'),
+                const Text(':'),
                 _TimerColumn(
                   key: minutesColumnKey,
-                  value: 34,
+                  value: model.remaining.inMinutes % 60,
                 ),
-                Text(':'),
+                const Text(':'),
                 _TimerColumn(
                   key: secondsColumnKey,
-                  value: 56,
+                  value: model.remaining.inSeconds % 60,
                 ),
               ],
             ),
@@ -62,7 +70,7 @@ class _TimerColumn extends StatelessWidget {
     return Column(
       mainAxisSize: MainAxisSize.min,
       children: [
-        Text(value.toString()),
+        Text(NumberFormat("00").format(value)),
       ],
     );
   }
