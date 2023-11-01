256ad11596d185be874b67cc0ff06a61794da8bf
Dennis Loose <dennis.loose@canonical.com>
2023-11-01 12:35:09 +0100

    register desktop notification service

--- timerapp/lib/main.dart
+++ timerapp/lib/main.dart
@@ -1,7 +1,14 @@
+import 'package:desktop_notifications/desktop_notifications.dart';
 import 'package:flutter/widgets.dart';
+import 'package:ubuntu_service/ubuntu_service.dart';
 
 import 'timer_app.dart';
 
 void main() {
+  registerService(
+    NotificationsClient.new,
+    dispose: (service) => service.close(),
+  );
+
   return runApp(const TimerApp());
 }
